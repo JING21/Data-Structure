abcb817a642d7821bdf6e6e78fa4a3d5ff29dc4b
zhoujing <zhoujing@cmss.chinamobile.com>
2020-08-10 14:31:25 +0800

    add BubbleSort

--- Sort/BubbleSort/BubbleSort.go
+++ Sort/BubbleSort/BubbleSort.go
@@ -4,19 +4,21 @@ import "fmt"
 
 func BubbleSort(slice []int){
 	fmt.Println("Before sort ",slice)
-	//for i := 0; i < len(slice); i++ {
-	//
-	//}
-}
-
-func swap(a,b,temp int){
-	temp = a
-	a = b
-	b = temp
+	temp := 0 //临时变量(用于做交换)
+	for i := 0; i < len(slice)-1; i++ {
+		for j := 0; j < len(slice)-1-i; j++ {
+			if (slice)[j] > (slice)[j+1] {
+				//交换
+				temp = (slice)[j]
+				(slice)[j] = (slice)[j+1]
+				(slice)[j+1] = temp
+			}
+		}
+	}
+	fmt.Println("排序后arr=", slice)
 }
 
 func main() {
-	var slice []int = []int{17,33,54,67,88}
-	fmt.Println(len(slice))
+	var slice []int = []int{30,55,27,19,80,77,45,21,18,44}
 	BubbleSort(slice)
 }
