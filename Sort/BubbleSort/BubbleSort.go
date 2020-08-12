ffcc59e08fb8653c54b90b94352263ba037d8af4
zhoujing <zhoujing@cmss.chinamobile.com>
2020-08-12 19:45:29 +0800

    Add selectSort

--- Sort/BubbleSort/BubbleSort.go
+++ Sort/BubbleSort/BubbleSort.go
@@ -21,4 +21,20 @@ func BubbleSort(slice []int){
 func main() {
 	var slice []int = []int{30,55,27,19,80,77,45,21,18,44}
 	BubbleSort(slice)
+	//var slice []int = []int{2,7,11,15}
+	//r := twoSum(slice,9)
+	//fmt.Println(r)
 }
+
+//func twoSum(nums []int, target int) []int {
+//	result := []int{}
+//	m := make(map[int]int)
+//	for i,k := range nums {
+//		if value,exist := m[target-k];exist {
+//			result = append(result,value)
+//			result = append(result,i)
+//		}
+//		m[k] = i
+//	}
+//	return result
+//}
