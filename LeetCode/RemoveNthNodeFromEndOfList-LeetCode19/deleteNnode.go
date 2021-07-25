1c47a6aa982477c6eae4cda9c81df2078a506988
JING <zj256038@qq.com>
2021-07-25 15:38:49 +0800

    add delete nth node from the end of the link-list

---
+++ LeetCode/RemoveNthNodeFromEndOfList-LeetCode19/deleteNnode.go
@@ -0,0 +1,25 @@
+package main
+
+type ListNode struct {
+     Val int
+     Next *ListNode
+}
+
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+	dummy := &ListNode{0,nil}
+	dummy.Next = head
+	length := 0
+	first := head
+	for(first != nil){
+		length++
+		first = first.Next
+	}
+	length = length-n
+	first = dummy
+	for (length >0){
+		length--
+		first = first.Next
+	}
+	first.Next = first.Next.Next
+	return dummy.Next
+}
---
+++ LinkList/deleteNnode.go
@@ -0,0 +1,21 @@
+package main
+
+
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+	dummy := &ListNode{0,nil}
+	dummy.Next = head
+	length := 0
+	first := head
+	for(first != nil){
+		length++
+		first = first.Next
+	}
+	length = length-n
+	first = dummy
+	for (length >0){
+		length--
+		first = first.Next
+	}
+	first.Next = first.Next.Next
+	return dummy.Next
+}
