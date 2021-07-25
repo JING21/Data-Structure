689f934ef1183a844e0bfc84fcb200aaebef7f93
JING <zj256038@qq.com>
2021-07-25 19:57:56 +0800

    add LeetCode-876

---
+++ LinkList/getMidNode.go
@@ -0,0 +1,15 @@
+package main
+
+
+func middleNode(head *ListNode) *ListNode {
+	if head.Next == nil {
+		return head
+	}
+	fast := head
+	slow := head
+	for (fast != nil && fast.Next != nil) {
+		fast = fast.Next.Next
+		slow = slow.Next
+	}
+	return slow
+}
