5140f8afcc10850c2bbb774e9f946659a270a78a
JING <zj256038@qq.com>
2021-07-25 19:28:00 +0800

    add second soultion for LeetCode-19

--- LinkList/deleteNnode.go
+++ LinkList/deleteNnode.go
@@ -19,3 +19,17 @@ func removeNthFromEnd(head *ListNode, n int) *ListNode {
 	first.Next = first.Next.Next
 	return dummy.Next
 }
+
+func removeNthFromEnd2(head *ListNode, n int) *ListNode {
+	dummy := &ListNode{0,nil}
+	dummy.Next = head
+	fast, slow := head, dummy
+	for i := 0; i <n ; i++ {
+		fast = fast.Next
+	}
+	for ; fast != nil; fast = fast.Next {
+		slow = slow.Next
+	}
+	slow.Next = slow.Next.Next
+	return dummy.Next
+}
