68fb8d9deb19d14ba860b465b4deb851cad3674a
JING <zj256038@qq.com>
2024-03-20 22:13:09 +0800

    add leetCode46

--- LeetCode/BackTracking/LeetCode46-Permutations/permute.go
+++ LeetCode/BackTracking/LeetCode46-Permutations/permute.go
@@ -9,6 +9,26 @@ var (
 func permute(nums []int) [][]int {
 	result, path = make([][]int, 0), make([]int, 0, len(nums))
 	used = make([]bool, len(nums))
-	backTracking()
+	backTracking(nums, 0)
 	return result
+}
+
+func backTracking(nums []int, level int) {
+	if level == len(nums) {
+		tmp := make([]int, len(path))
+		copy(tmp, path)
+		result = append(result, tmp)
+		return
+	}
+
+	for i := 0; i < len(nums); i++ {
+		if used[i] {
+			continue
+		}
+		path = append(path, nums[i])
+		used[i] = true
+		backTracking(nums, level+1)
+		path = path[:len(path)-1]
+		used[i] = false
+	}
 }
